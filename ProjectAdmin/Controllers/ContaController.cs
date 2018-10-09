497ebc15b488f96f679b48dbe28b981c2460c12a
Jamite André <jaimemagico16@hotmail.com>
2018-10-09 21:08:47 +0100

    sim

--- ProjectAdmin/Controllers/ContaController.cs
+++ ProjectAdmin/Controllers/ContaController.cs
@@ -58,6 +58,36 @@ namespace ProjectAdmin.Controllers
             return RedirectToAction("Index");// PRG
         }
 
+        [HttpPost]
+        public async Task<ActionResult> UploadPhotoCapa(HttpPostedFileBase file, string nome, string id)
+        {
+            var Usuario = db.Users.Find(id);
+            if (file != null && file.ContentLength > 0)
+            {
+                var user = id;
+                var username = user;
+                var fileExt = Path.GetExtension(file.FileName);
+                var fnm = username + ".png";
+                if (fileExt.ToLower().EndsWith(".png") || fileExt.ToLower().EndsWith(".jpg") || fileExt.ToLower().EndsWith(".gif"))// Important for security if saving in webroot
+                {
+                    var filePath = HostingEnvironment.MapPath("~/Imagens/") + fnm;
+                    var directory = new DirectoryInfo(HostingEnvironment.MapPath("~/Imagens/"));
+                    if (directory.Exists == false)
+                    {
+                        directory.Create();
+                    }
+                    ViewBag.FilePath = filePath.ToString();
+                    file.SaveAs(filePath);
+                    return RedirectToAction("Definicoes", "Conta", new { id = id });
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");// PRG
+        }
+
         #endregion Upload Photo
     }
 }
--- ProjectAdmin/Controllers/HomeController.cs
+++ ProjectAdmin/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using ProjectAdmin.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +13,32 @@ namespace ProjectAdmin.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
+        {
+        }
+
+        public HomeController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+           
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+
         public ActionResult Index()
         {
             return View();
@@ -34,5 +64,35 @@ namespace ProjectAdmin.Controllers
             file.SaveAs(Path.Combine(Server.MapPath("~/img"), filePath));
             return Json("Carregado com Sucesso");
         }
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Isso não conta falhas de login em relação ao bloqueio de conta
+            // Para permitir que falhas de senha acionem o bloqueio da conta, altere para shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToAction("Index", "Feed");
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Tentativa de login inválida.");
+                    return View(model);
+            }
+        }
+
     }
 }
